d7f5b978006bdab6dfe3af040700290e00ccf595
PureWindSN <1983457078@qq.com>
2025-05-01 11:52:02 +0800

    inittial backend

---
+++ PurchaseBackend/src/main/java/com/wind/purchasebackend/PurchaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wind.purchasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseBackendApplication.class, args);
+    }
+
+}
---
+++ PurchaseBackend/src/test/java/com/wind/purchasebackend/PurchaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wind.purchasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
